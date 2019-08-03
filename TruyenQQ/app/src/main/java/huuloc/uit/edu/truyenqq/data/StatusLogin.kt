7d3f93e75bfbf2809d9943c7209e51d4bed09069
Trần Hữu Lộc <loctran0169@gmail.com>
2019-08-03 21:24:08 +0700

    xóa hết

--- TruyenQQ/app/src/main/java/huuloc/uit/edu/truyenqq/data/StatusLogin.kt
+++
@@ -1,8 +0,0 @@
-package huuloc.uit.edu.truyenqq.data
-
-import com.google.gson.annotations.SerializedName
-
-data class StatusLogin(
-    @SerializedName("status")
-    var status: Int
-)
--- TruyenQQ/app/src/main/java/huuloc/uit/edu/truyenqq/network/test.java
+++
@@ -1,5 +0,0 @@
-package huuloc.uit.edu.truyenqq.network;
-
-public class test {
-}
-
